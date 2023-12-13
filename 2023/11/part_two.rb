01f8d4e9ae36c02a30530d89c3f4e13e24f73c0f
Matthew Miner <mminer237@gmail.com>
2023-12-12 18:53:17 -0600

    Added 2023-12-11

---
+++ 2023/11/part_two.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+map = IO.readlines("input.txt")
+	.map { |line| line.chomp.split("") }
+
+empty_rows = Array.new(map.length, true)
+empty_columns = Array.new(map.first.length, true)
+galaxies = []
+map.each_with_index do |row, y|
+	row.each_with_index do |cell, x|
+		if cell == "#"
+			empty_rows[y] = false
+			empty_columns[x] = false
+			galaxies.push([x, y])
+		end
+	end
+end
+
+galaxies.map! { |galaxy| [
+	galaxy[0] + (empty_columns.select.with_index { |empty, x| empty && x < galaxy[0] }).length * (1_000_000 - 1),
+	galaxy[1] + (empty_rows.select.with_index { |empty, y| empty && y < galaxy[1] }).length * (1_000_000 - 1)
+]}
+
+sum = 0
+galaxies.each_with_index do |galaxy_one, i|
+	galaxies[i + 1..-1].each do |galaxy_two|
+		sum += (galaxy_one[0] - galaxy_two[0]).abs + (galaxy_one[1] - galaxy_two[1]).abs
+	end
+end
+puts sum
