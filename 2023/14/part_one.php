c10339551f381e753d9b8312da1274997fb09c2f
Matthew Miner <mminer237@gmail.com>
2023-12-20 22:48:20 -0600

    Add day 14, part 1

---
+++ 2023/14/part_one.php
@@ -0,0 +1,29 @@
+#!/usr/bin/env php
+<?php
+declare(strict_types=1);
+
+$sum = 0;
+$handle = fopen("input.txt", "r");
+$columns = array_fill(0, strlen(fgets($handle)), 0);
+rewind($handle);
+$lines = 0;
+$rocks = 0;
+while (($line = fgets($handle)) != false) {
+	$lines++;
+	for ($i = 0; $i < strlen($line); $i++) {
+		switch ($line[$i]) {
+			case 'O':
+				$sum += $columns[$i]++;
+				$rocks++;
+				break;
+			case '#':
+				$columns[$i] = $lines;
+				break;
+		}
+	}
+}
+fclose($handle);
+
+$sum = $rocks * $lines - $sum;
+
+echo $sum . PHP_EOL;
